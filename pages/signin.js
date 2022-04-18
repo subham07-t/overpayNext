ca819e097fe1c57240d15253dd4735a8e3184052
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-04-18 20:34:55 +0530

    second commit

--- pages/signin.js
+++ pages/signin.js
@@ -3,68 +3,68 @@ const signin = () => {
   return (
     <>
       {/* <!-- Main section --> */}
-      <div class="flex items-start justify-between h-screen p-4">
+      <div className="flex items-start justify-between h-screen p-4">
         {/* <!-- left section --> */}
-        <div class=" w-full lg:w-8/12 h-full flex flex-col  sm:px-8">
+        <div className=" w-full lg:w-8/12 h-full flex flex-col  sm:px-8">
           {/* <!-- Header section --> */}
-          <div class="  w-32 my-2 ">
-            <img class=" w-full" src="/imgs/Logo.svg" alt="" />
+          <div className="  w-32 my-2 ">
+            <img className=" w-full" src="/imgs/Logo.svg" alt="" />
           </div>
 
           {/* <!-- Mid section --> */}
-          <div class=" flex-1 flex flex-col items-center justify-center">
-            <div class="  flex flex-col items-center justify-center  w-full max-w-md my-4">
+          <div className=" flex-1 flex flex-col items-center justify-center">
+            <div className="  flex flex-col items-center justify-center  w-full max-w-md my-4">
               {/* <!-- Tittle section --> */}
-              <div class="text-center">
-                <h1 class="text-3xl font-bold">Sign in to Overpay</h1>
-                <p class="text-md my-4">Send, spend and save smarter</p>
+              <div className="text-center">
+                <h1 className="text-3xl font-bold">Sign in to Overpay</h1>
+                <p className="text-md my-4">Send, spend and save smarter</p>
               </div>
 
               {/* <!-- Social section --> */}
-              <div class=" sm:flex justify-between w-full">
-                <div class="flex border p-4 rounded-lg hover:border-blue-300">
+              <div className=" sm:flex justify-between w-full">
+                <div className="flex border p-4 rounded-lg hover:border-blue-300">
                   <img src="/imgs/google.svg" alt="" />
-                  <p class="mx-2">Sign In with Google</p>
+                  <p className="mx-2">Sign In with Google</p>
                 </div>
-                <div class="flex border p-4 rounded-lg hover:border-blue-300">
+                <div className="flex border p-4 rounded-lg hover:border-blue-300">
                   <img src="/imgs/apple.svg" alt="" />
-                  <p class=" mx-2">Sign In with Apple</p>
+                  <p className=" mx-2">Sign In with Apple</p>
                 </div>
               </div>
 
               {/* <!-- Or section --> */}
-              <div class="flex items-center w-full py-2">
-                <div class="flex-1 border"></div>
-                <p class="mx-2">Or with email</p>
-                <div class="flex-1 border"></div>
+              <div className="flex items-center w-full py-2">
+                <div className="flex-1 border"></div>
+                <p className="mx-2">Or with email</p>
+                <div className="flex-1 border"></div>
               </div>
 
               {/* <!-- Form section --> */}
-              <form class=" my-4 w-full" action="">
-                <div class=" border p-4 rounded-xl my-2 hover:border-blue-300">
+              <form className=" my-4 w-full" action="">
+                <div className=" border p-4 rounded-xl my-2 hover:border-blue-300">
                   <input
                     type="text"
-                    class="outline-0"
+                    className="outline-0"
                     placeholder="Username or email"
                   />
                 </div>
-                <div class=" flex border p-4 rounded-xl justify-between my-2 hover:border-blue-300">
+                <div className=" flex border p-4 rounded-xl justify-between my-2 hover:border-blue-300">
                   <input
                     type="password"
-                    class="outline-0"
+                    className="outline-0"
                     placeholder="Password"
                   />
                   <img src="/imgs/eye-off.svg" alt="" />
                 </div>
-                <div class="flex justify-between w-full">
+                <div className="flex justify-between w-full">
                   <div>
-                    <input type="checkbox" class="rounded-full" />
+                    <input type="checkbox" className="rounded-full" />
                     Remember me
                   </div>
                   <a href="">Forgot password?</a>
                 </div>
-                <div class="my-4">
-                  <button class="w-full bg-blue-700 text-white rounded-xl p-4">
+                <div className="my-4">
+                  <button className="w-full bg-blue-700 text-white rounded-xl p-4">
                     Sign In
                   </button>
                 </div>
@@ -85,15 +85,19 @@ const signin = () => {
           </div>
 
           {/* <!-- Footer section --> */}
-          <div class="flex justify-between items-center">
+          <div className="flex justify-between items-center">
             <a href="">Privacy policy</a>
             <a href="">Copyright 2022</a>
           </div>
         </div>
 
         {/* <!-- right section --> */}
-        <div class="  hidden lg:flex w-4/12 h-full">
-          <img class=" w-full h-full object-fill" src="/imgs/bg.png" alt="" />
+        <div className="  hidden lg:flex w-4/12 h-full">
+          <img
+            className=" w-full h-full object-fill"
+            src="/imgs/bg.png"
+            alt=""
+          />
         </div>
       </div>
     </>
--- pages/signup.js
+++ pages/signup.js
@@ -3,80 +3,91 @@ const signup = () => {
   return (
     <>
       {/* <!-- Main section --> */}
-      <div class="flex items-start justify-between h-screen p-4">
+      <div className="flex items-start justify-between h-screen p-4">
         {/* <!-- left section --> */}
-        <div class="  hidden lg:flex w-4/12 h-full">
-          <img class=" w-full h-full object-fill" src="/imgs/bg1.jpg" alt="" />
+        <div className="  hidden lg:flex w-4/12 h-full">
+          <img
+            className=" w-full h-full object-fill"
+            src="/imgs/bg1.jpg"
+            alt=""
+          />
         </div>
 
         {/* <!-- right section --> */}
-        <div class=" w-full lg:w-8/12 h-full flex flex-col  sm:px-8">
+        <div className=" w-full lg:w-8/12 h-full flex flex-col  sm:px-8">
           {/* <!-- Top section --> */}
-          <div class=" flex-1 flex flex-col items-center justify-center">
-            <div class="  flex flex-col items-center justify-center  w-full max-w-md my-4">
+          <div className=" flex-1 flex flex-col items-center justify-center">
+            <div className="  flex flex-col items-center justify-center  w-full max-w-md my-4">
               {/* <!-- Tittle section --> */}
-              <div class="text-center">
-                <h1 class="text-3xl font-bold">Sign up for an account</h1>
-                <p class="text-md my-4">Send, spend and save smarter</p>
+              <div className="text-center">
+                <h1 className="text-3xl font-bold">Sign up for an account</h1>
+                <p className="text-md my-4">Send, spend and save smarter</p>
               </div>
 
               {/* <!-- Social section --> */}
-              <div class=" sm:flex justify-between w-full">
-                <div class="flex border p-4 rounded-lg hover:border-blue-300">
+              <div className=" sm:flex justify-between w-full">
+                <div className="flex border p-4 rounded-lg hover:border-blue-300">
                   <img src="/imgs/google.svg" alt="" />
-                  <p class="mx-2">Sign In with Google</p>
+                  <p className="mx-2">Sign In with Google</p>
                 </div>
-                <div class="flex border p-4 rounded-lg hover:border-blue-300">
+                <div className="flex border p-4 rounded-lg hover:border-blue-300">
                   <img src="/imgs/apple.svg" alt="" />
-                  <p class=" mx-2">Sign In with Apple</p>
+                  <p className=" mx-2">Sign In with Apple</p>
                 </div>
               </div>
 
               {/* <!-- Or section --> */}
-              <div class="flex items-center w-full py-2">
-                <div class="flex-1 border"></div>
-                <p class="mx-2">Or with email</p>
-                <div class="flex-1 border"></div>
+              <div className="flex items-center w-full py-2">
+                <div className="flex-1 border"></div>
+                <p className="mx-2">Or with email</p>
+                <div className="flex-1 border"></div>
               </div>
 
               {/* <!-- form section --> */}
-              <form class=" my-4 w-full" action="">
-                <div class=" sm:flex justify-between w-full">
-                  <div class=" border p-4 rounded-lg hover:border-blue-300">
+              <form className=" my-4 w-full" action="">
+                <div className=" sm:flex justify-between w-full">
+                  <div className=" border p-4 rounded-lg hover:border-blue-300">
                     <input
                       type="text"
-                      class="outline-0"
+                      className="outline-0"
                       placeholder="First name"
                     />
                   </div>
-                  <div class=" border p-4 rounded-lg hover:border-blue-300">
+                  <div className=" border p-4 rounded-lg hover:border-blue-300">
                     <input
                       type="text"
-                      class="outline-0"
+                      className="outline-0"
                       placeholder="Last name"
                     />
                   </div>
                 </div>
-                <div class=" border p-4 rounded-xl my-2 hover:border-blue-300">
-                  <input type="text" class="outline-0" placeholder="Email" />
+                <div className=" border p-4 rounded-xl my-2 hover:border-blue-300">
+                  <input
+                    type="text"
+                    className="outline-0"
+                    placeholder="Email"
+                  />
                 </div>
-                <div class=" flex border p-4 rounded-xl justify-between my-2 hover:border-blue-300">
+                <div className=" flex border p-4 rounded-xl justify-between my-2 hover:border-blue-300">
                   <input
                     type="password"
-                    class="outline-0"
+                    className="outline-0"
                     placeholder="Password"
                   />
                   <img src="/imgs/eye-off.svg" alt="" />
                 </div>
-                <div class="flex justify-between w-full">
+                <div className="flex justify-between w-full">
                   <p>
                     By creating an account, you agreeing to our{" "}
-                    <a class="font-bold">Privacy Policy</a>, and{" "}
-                    <a class="font-bold">Electronics Communication Policy</a>.
+                    <a className="font-bold">Privacy Policy</a>, and{" "}
+                    <a className="font-bold">
+                      Electronics Communication Policy
+                    </a>
+                    .
                   </p>
                 </div>
-                <div class="my-4">
-                  <button class="w-full bg-blue-700 text-white rounded-xl p-4">
+                <div className="my-4">
+                  <button className="w-full bg-blue-700 text-white rounded-xl p-4">
                     Sign In
                   </button>
                 </div>
@@ -97,7 +108,7 @@ const signup = () => {
           </div>
 
           {/* <!-- Footer section --> */}
-          <div class="flex justify-between items-center">
+          <div className="flex justify-between items-center">
             <a href="">Privacy policy</a>
             <a href="">Copyright 2022</a>
           </div>
